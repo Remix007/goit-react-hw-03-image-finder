dd0a5378cf8e207d75100a9ae6f2347560796479
Remix007 <forvard16back@gmail.com>
2023-02-20 01:26:07 +0200

    ends bar

---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,55 @@
+import { Component } from 'react';
+import { ImSearch } from 'react-icons/im';
+import { toast } from 'react-toastify';
+import PropTypes from 'prop-types';
+
+import style from './SearchBar.module.css';
+
+class SearchBar extends Component {
+  state = {
+    imageName: '',
+  };
+
+  hendleNameChange = event => {
+    this.setState({ imageName: event.currentTarget.value.toLowerCase() });
+  };
+
+  hanedleSubmit = event => {
+    event.preventDefault();
+
+    if (this.state.imageName.trim() === '') {
+      toast('Input image name');
+      return;
+    }
+    this.props.onSubmit(this.state.imageName);
+
+    this.setState({ imageName: '' });
+  };
+  render() {
+    return (
+      <header className={style.searchbar}>
+        <form className={style.form} onSubmit={this.hanedleSubmit}>
+          <button type="submit" className={style.button}>
+            <ImSearch />
+          </button>
+
+          <input
+            className={style.input}
+            type="text"
+            name="imageName"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+            value={this.state.imageName}
+            onChange={this.hendleNameChange}
+          />
+        </form>
+      </header>
+    );
+  }
+}
+export default SearchBar;
+
+SearchBar.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
