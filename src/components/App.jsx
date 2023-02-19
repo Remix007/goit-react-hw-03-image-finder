fa962fe9af4cf2ed7c96afcafea97b4eb5e9e230
Remix007 <forvard16back@gmail.com>
2023-02-20 01:29:22 +0200

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React from 'react';
+import { Component } from 'react';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import SearchBar from './SearchBar';
+import Gallary from './Gallary';
+
+class App extends Component {
+  state = {
+    imageName: '',
+  };
+  hendleFormSearchSubmit = imageName => {
+    this.setState({ imageName });
+  };
+  render() {
+    const { imageName } = this.state;
+    return (
+      <>
+        <ToastContainer position="top-left" autoClose={2000} />
+        <SearchBar onSubmit={this.hendleFormSearchSubmit} />
+        <Gallary imageName={imageName} />
+      </>
+    );
+  }
+}
+export default App;
