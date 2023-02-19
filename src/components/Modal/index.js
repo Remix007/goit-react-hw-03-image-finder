5cce2588f3d2ab4e54facea312e5d3f910c10a3f
Remix007 <forvard16back@gmail.com>
2023-02-20 00:41:39 +0200

    fix modal

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,16 +1,1 @@
 export { default } from './Modal.jsx';
-.overlay {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100vw;
-    height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background-color: rgba(0, 0, 0, 0.8);
-  }
-  .modal__content {
-    max-width: calc(100vw - 48px);
-  max-height: calc(100vh - 24px);
-}
