23a5a39547ae103fa00e553bef0ca78d08539608
Remix007 <forvard16back@gmail.com>
2023-02-20 00:40:21 +0200

    modal

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { createPortal } from 'react-dom';
+import PropTypes from 'prop-types';
+import style from './Modal.module.css';
+
+const modalRoot = document.querySelector('#modal-root');
+class Modal extends Component {
+  componentDidMount() {
+    window.addEventListener('keydown', this.hendleKeyDown);
+  }
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.hendleKeyDown);
+  }
+  hendleKeyDown = event => {
+    if (event.code === 'Escape') {
+      this.props.onClose();
+    }
+  };
+  hendleBackDropClick = event => {
+    if (event.currentTarget === event.target) {
+      this.props.onClose();
+    }
+  };
+  render() {
+    return createPortal(
+      <div className={style.overlay} onClick={this.hendleBackDropClick}>
+        <div className={style.modal__content}>{this.props.children}</div>
+      </div>,
+      modalRoot
+    );
+  }
+}
+
+export default Modal;
+
+Modal.propTypes = {
+  onClose: PropTypes.func.isRequired,
+};
